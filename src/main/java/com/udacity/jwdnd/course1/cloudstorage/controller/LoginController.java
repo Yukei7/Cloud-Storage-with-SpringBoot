35225c1f7abcdda4ea5d7fa1bc01ce6d4d796831
Yukei7 <yukei0509@gmail.com>
2021-10-11 01:58:09 -0400

    Signup and Login features

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+    @GetMapping
+    public String loginView(@RequestParam(required = false) String message, Model model) {
+        if (message != null) {
+            model.addAttribute("param.message", message);
+        }
+        return "login";
+    }
+}
